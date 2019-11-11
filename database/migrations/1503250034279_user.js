72e3a5209f2f27bf085bdbd8433390023651d69f
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2019-11-10 22:51:09 -0300

    coupon_orders relationship

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,10 +7,8 @@ class UserSchema extends Schema {
   up() {
     this.create('users', table => {
       table.increments()
-      table
-        .string('name', 80)
-        .notNullable()
-        .unique()
+      table.string('name', 80).notNullable()
+      table.string('surname', 200).notNullable()
       table
         .string('email', 254)
         .notNullable()
